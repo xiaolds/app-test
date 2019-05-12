5ad726ccd534eb4b9d15717f2f2056f8b13e9971
冬笙(david.lds) <david.lds@antfin.com>
2019-05-12 22:05:24 +0800

    Updates

---
+++ precache-manifest.796cee9cb4b07830db7cdb0dc75cb0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a79f43a2fef6d007bb56d94db8199d5",
+    "url": "/app-test/index.html"
+  },
+  {
+    "revision": "a8dee6e63bd5c4e5bf2a",
+    "url": "/app-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/app-test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a8dee6e63bd5c4e5bf2a",
+    "url": "/app-test/static/js/main.9b0ffc7b.chunk.js"
+  },
+  {
+    "revision": "ae75d3d0d031fa40b15f",
+    "url": "/app-test/static/js/runtime~main.7af4987c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
